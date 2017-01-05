1bbb3f8d8bfb5cda5dc74d18ea3696e7aef8e50c
Björn <bjoern.seyfert@web.de>
2017-01-05 19:03:22 +0100

    Björn 05.01.17 Listener Versuch

--- app/src/main/java/com/example/expresso/myapplication/run.java
+++ app/src/main/java/com/example/expresso/myapplication/run.java
@@ -1,30 +1,55 @@
 package com.example.expresso.myapplication;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.widget.Toast;
+
+
+import static android.bluetooth.BluetoothAdapter.*;
 
 /**
  * hergestellt von Björn on 15.12.16.
  */
 public class run extends AppCompatActivity {
-
     Button r_up;
     Button r_down;
     Button l_up;
     Button l_down;
+//Versuch Nr.1
+/*
+    protected void btcheck(Intent intent) {
+        String action = intent.getAction();
 
+        if(BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action))
 
+        {
+            Intent myIntent = new Intent(run.this, MainActivity.class);
+            startActivity(myIntent);
+        }
+    }
 
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+//Hier beginnt versuch zwei nach:
+        // http://de.androids.help/q24586
+        IntentFilter filter1 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        this.registerReceiver(mReceiver, filter1);
+//Ende versuch zwei (Weiter geht es unten)
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run);
+        setContentView(R.layout.activity_run)   ;
+
         r_up = (Button) findViewById(R.id.r_up);
         r_down = (Button) findViewById(R.id.r_down);
         l_up = (Button) findViewById(R.id.l_up);
@@ -98,5 +123,23 @@ public class run extends AppCompatActivity {
                 return false;
             }
         });
+        }
+    //Der Listener ob sich der State auf DIsconnected befindet.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                 //Device found
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                Intent myIntent = new Intent(run.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        }
+    };
     }
-}
+
+
